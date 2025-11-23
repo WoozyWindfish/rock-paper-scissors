b05ab9286f8306b86599d3abb4415dc64e057aaa
WoozyWindfish <nicenglehardt@gmail.com>
2025-11-24 01:26:57 +0900

    fix scoring syntax

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 
 function getRandomInt(min, max) {
   const minCeiled = Math.ceil(min);
@@ -17,4 +20,43 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice()); 
+function getHumanChoice() {
+  let input = prompt ('Rock, Paper, or Scissors?');
+  input = input.toLowerCase();
+  return input;
+}
+
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+  
+  if (humanChoice === computerChoice) {
+    return "It's a tie!";
+  } 
+  else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice == "rock")  ||
+    (humanChoice === "scissors" && computerChoice == "paper")
+  ) {
+    humanScore++;
+    return `You win! ${humanChoice} beats ${computerChoice}.`;
+  }
+
+  else {
+    computerScore++;
+    return `You lose! ${computerChoice} beats ${humanChoice}.`;
+  }
+}
+
+
+const humanSelection = getHumanChoice();
+
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+
+function playGame() {
+  playRound(humanSelection, computerSelection);
+
+}
